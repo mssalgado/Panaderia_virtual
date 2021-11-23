a81e4fb1935e762aacc161bfe40386d51bd38e5d
mssalgado <90221320+mssalgado@users.noreply.github.com>
2021-11-23 00:18:51 -0500

    Frontend

---
+++ FRONTEND/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
+import { StockListComponent } from './stock/stock-list/stock-list.component';
+import { StockFormComponent } from './stock/stock-form/stock-form.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    UserFormComponent,
+    StockListComponent,
+    StockFormComponent,
+    NavbarComponent,
+    UserLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
