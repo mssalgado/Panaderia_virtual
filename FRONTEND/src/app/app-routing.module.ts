bcc8426862ea8c20c6b0eab2ec3a20abd2b58db5
whariza <94945576+whariza@users.noreply.github.com>
2021-12-01 12:21:14 -0500

    probando rutas
    probando rutas de navegacion

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StockListComponent } from './stock/stock-list/stock-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stock', component: StockListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
